12c8f0a94ec0c385c963930b44323305391470e4
0aids <aidanlldanu@gmail.com>
2026-03-26 16:49:47 +1100

    perf: batched snapshots and searching in scanner example'

--- examples/scanner.cpp
+++ examples/scanner.cpp
@@ -26,14 +26,19 @@ int main(int argc, const char** argv)
                     .FilterActiveRegions(pid)
                     .BreakIntoChunks(0x10000000);
     cout << "Num maps: " << maps.size() << endl;
-    auto lambda = [&maps, &pid, &matchString](auto analyzer) mutable
+    auto snapAndFind =
+        [pid](const rmf::types::MemoryRegionProperties& mrp,
+              const std::string&                        str)
     {
-        auto start     = chrono::steady_clock::now();
-        auto snapshots = analyzer.Execute(
-            rmf::types::MemorySnapshot::Make, maps, pid);
+        auto snap = rmf::types::MemorySnapshot::Make(mrp, pid);
+        return rmf::op::findString(snap, str);
+    };
+    auto lambda = [&snapAndFind, &maps, &pid,
+                   &matchString](auto analyzer) mutable
+    {
+        auto start = chrono::steady_clock::now();
         auto results =
-            analyzer
-                .Execute(rmf::op::findString, snapshots, matchString)
+            analyzer.Execute(snapAndFind, maps, matchString)
                 .flatten();
         cout << "Results length: " << results.size() << endl;
         cout << "Results type: " << typeid(results).name() << endl;
@@ -48,4 +53,25 @@ int main(int argc, const char** argv)
     }
     cout << "Time taken " << thread::hardware_concurrency() - 1
          << " threads: " << time1 << endl;
+
+    // Trying again with no analyzer.
+    maps = rmf::utils::getMapsFromPid(pid)
+               .FilterHasPerms("r")
+               .FilterActiveRegions(pid)
+               .BreakIntoChunks(0x10000000);
+    auto start = chrono::steady_clock::now();
+    rmf::types::MemoryRegionPropertiesVec results;
+    for (const auto& map : maps)
+    {
+        auto snap   = rmf::types::MemorySnapshot::Make(map, pid);
+        auto result = rmf::op::findString(snap, matchString);
+        for (auto&& r : result)
+        {
+            results.emplace_back(std::move(r));
+        }
+    }
+    std::chrono::duration<double> timeTaken =
+        chrono::steady_clock::now() - start;
+    cout << "Time taken S.T: " << timeTaken << endl;
+    cout << "Num results: " << results.size() << endl;
 }
