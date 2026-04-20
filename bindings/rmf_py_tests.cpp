55d96a4f1ecb4aa8dfa28cda77bd38a74a00a386
0aids <aidanlldanu@gmail.com>
2026-04-20 16:09:24 +1000

    refactor preparation

--- bindings/rmf_py_tests.cpp
+++
@@ -1,8 +0,0 @@
-#include <pybind11/pybind11.h>
-
-namespace py = pybind11;
-
-PYBIND11_MODULE(rmf_test_py, m, py::mod_gil_not_used())
-{
-    m.doc() = "Helpers for testing for rmf_py";
-}
--- rmf_py/__init__.py
+++
@@ -1,4 +0,0 @@
-from .rmf_core_py import *
-
-# Write some generic code here replacing
-# templated code such as Analyzer::execute.
