bcf3289596e232ae86961df965307df350e27d12
0aids <aidanlldanu@gmail.com>
2026-04-24 14:37:55 +1000

    tests: more testing for sanity check conversions

--- tests/map.cpp
+++ tests/map.cpp
@@ -1,3 +1,4 @@
+#include "rmf/snapshot.hpp"
 #include <gtest/gtest.h>
 #include <print>
 #include <rmf/logging/logging.hpp>
@@ -17,5 +18,36 @@ TEST(map, BasicRegion)
     mf::Node<mf::Map> test2;
     println("{}", test1.map.regionName_sp.use_count());
     println("{}", std::string(test1));
+    EXPECT_EQ(test1.map, test2.map);
+}
+
+TEST(map, MapSnapConversion)
+{
+    auto name = std::make_shared<const std::string>("hello world");
+    mf::Detail::MapData data = {
+        .parentAddress   = 0xff,
+        .parentSize      = 0xffff,
+        .relativeAddress = 0xaa,
+        .relativeSize    = 0xff,
+        .regionName_sp   = name,
+    };
+    mf::Node<mf::Map, mf::Snapshot> test1;
+    mf::Node<mf::Map>               test2(test1);
+    // What in the holy heck.
+    EXPECT_EQ(test1.map, test2.map);
+}
+
+TEST(map, SnapMapConversion)
+{
+    auto name = std::make_shared<const std::string>("hello world");
+    mf::Detail::MapData data = {
+        .parentAddress   = 0xff,
+        .parentSize      = 0xffff,
+        .relativeAddress = 0xaa,
+        .relativeSize    = 0xff,
+        .regionName_sp   = name,
+    };
+    mf::Node<mf::Map>               test1;
+    mf::Node<mf::Map, mf::Snapshot> test2(test1);
     EXPECT_EQ(test1.map, test2.map);
 }
